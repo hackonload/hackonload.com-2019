8697b022c67f0ea247f49281f84588432cfb3233
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-30 00:57:19 +0530

    Basic website ready.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const faqs = require('./../data/faqs');
+
+const linksArray = [
+	{
+		title: "Home",
+		link: "#home"
+	},
+	{
+		title: "About",
+		link: "#about"
+	},
+	{
+		title: "Schedule",
+		link: "#schedule"
+	},
+	{
+		title: "Sponsors",
+		link: "#sponsors"
+	}
+];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', {title: 'Express'});
+	const data = {
+		title: "onLoad: Load your code",
+		links: linksArray,
+		faqs: faqs
+	};
+	res.render('index', data);
 });
 
 module.exports = router;
