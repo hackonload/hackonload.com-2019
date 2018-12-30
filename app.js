94ea5574e90208d73cf13be94f5f86d489e6f21d
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-30 12:29:48 +0530

    performance tweaks.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 
@@ -19,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
+app.use(compression());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
