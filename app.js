cc9f3962a39f44dd1da7a4f2d266a224284b6f8d
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-31 01:26:01 +0530

    performance improvements.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const minify = require('express-minify');
 const compression = require('compression');
 require('dotenv').config();
 
@@ -18,10 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(compression());
+app.use(minify());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use(compression());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
