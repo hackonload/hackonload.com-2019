3dcd07a14de91702bf428710b1ff6d6391164dea
Pushkar Anand <anandpushkar088@gmail.com>
2019-02-13 18:51:08 +0530

    testing email.

---
+++ data/participants-test.js
@@ -0,0 +1,17 @@
+const participants = [
+	{
+		name: "Pushkar Anand",
+		email: "anandpushkar088@gmail.com",
+		phone: "+918340589025",
+		slack_invite_sent: true
+	},
+	{
+		name: "Akshay GR",
+		email: "akshaygr4@gmail.com",
+		phone: "+91",
+		slack_invite_sent: true
+	},
+
+];
+
+module.exports = participants;
